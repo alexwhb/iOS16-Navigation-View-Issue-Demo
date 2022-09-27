0b48ea887b03fefc73393a2049dbaa501cdf084c
Alex Black <alex.black@blackstoneaudio.com>
2022-09-27 11:59:20 -0700

    Initial Commit

---
+++ DemoiOS16Issue/DemoiOS16IssueApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoiOS16IssueApp.swift
+//  DemoiOS16Issue
+//
+//  Created by Alexander Black on 9/27/22.
+//
+
+import SwiftUI
+
+@main
+struct DemoiOS16IssueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
